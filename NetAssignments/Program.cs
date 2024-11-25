0acec2c3837575eef038ff2d60591451389084b9
Nutan-Gyawali <nutangyawali01@gmail.com>
2024-11-25 16:06:21 +0545

    Swap two variables with no third variable

---
+++ NetAssignments/Program.cs
@@ -0,0 +1,17 @@
+﻿// Program to swap variables without using a temporary variable
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 20;
+        Console.WriteLine($"Before Swapping a = {a} and b = {b}");
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        Console.WriteLine($"Before Swapping a = {a} and b = {b}");
+
+    }
+}
