2d9db2b829d755fbdd4b185a2ef342cb5e3e880a
Nutan Gyawali <119723003+Nutan-Gyawali@users.noreply.github.com>
2024-12-02 20:37:13 +0545

    DynamicPolymorphism

--- NetAssignments/Program.cs
+++ NetAssignments/Program.cs
@@ -1,12 +1,26 @@
-﻿using NetAssignments;
+using NetAssignments;
 using System;
+using static NetAssignments.DynamicPolymorphism;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+
+        //Assignment 1 
         Loops loopsDemo = new Loops();
 
+        //Assignment 2
         Swap swapDemo = new Swap();
+
+        // Assignment 3 / Date : 12/2/2024
+        Animal myAnimal = new Animal();
+        Animal myCat = new Cat();
+        Animal myDog = new Dog();         
+           
+
+        myAnimal.Sound();  
+        myDog.Sound();
+        myCat.Sound();
     }
 }
